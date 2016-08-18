beb5efd1c8f96aa13b2486d2a641d48cd0ae12d4
董新 <xin.dong@dewmobile.net>
2016-08-18 14:16:30 +0800

    Initial Commit

---
+++ SeeMoney/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeeMoney
+//
+//  Created by douglas on 16/8/18.
+//  Copyright © 2016年 douglas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
