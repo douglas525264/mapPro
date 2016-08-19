81947b3382c5ff81d921a538785ad152f7dc3e91
董新 <xin.dong@dewmobile.net>
2016-08-19 16:15:46 +0800

    add some fun

---
+++ Pods/Target Support Files/ATMHud@dhoerl/ATMHud@dhoerl-umbrella.h
@@ -0,0 +1,12 @@
+#import <UIKit/UIKit.h>
+
+#import "ATMHud.h"
+#import "ATMHudDelegate.h"
+#import "ATMHudQueueItem.h"
+#import "ATMHudView.h"
+#import "ATMProgressLayer.h"
+#import "ATMTextLayer.h"
+
+FOUNDATION_EXPORT double ATMHud_dhoerlVersionNumber;
+FOUNDATION_EXPORT const unsigned char ATMHud_dhoerlVersionString[];
+
